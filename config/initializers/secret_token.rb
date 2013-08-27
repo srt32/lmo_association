bc8d9d866ccc5f242608e58a8246e8aa6c819594
Simon <simon.taranto@gmail.com>
2013-08-27 17:45:48 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lmo::Application.config.secret_token = 'e73aca44d4bc4421ffa58718f460a9c765ac1ae20847fe01dc150ff066fcf0be8634a818f9f55455a0a32a72e0c0e99d1c8630397a6c07df9a7620c77c3cbae0'
